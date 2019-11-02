2df2f7068c34ebb1d7a9b471d6415deed9987c09
ROHROCK <rohitsa40@gmail.com>
2019-11-02 14:16:33 +0530

    Update

--- HPC/Assignment No.4 (Vector)/vectorMul.cu
+++ HPC/Assignment No.4 (Vector)/vectorMul.cu
@@ -1,9 +1,9 @@
 #include<iostream>
 #include<time.h>
-#define SIZE 3
+#define SIZE 10000
 using namespace std;
 
-__global__ void mul(int *vect , int (*mat)[SIZE][SIZE] , int *res){
+__global__ void mul(int *vect , int (*mat)[SIZE][SIZE] , long *res){
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     res[i] = 0;
     for(int j = 0 ; j < SIZE ; j++)
@@ -13,13 +13,13 @@ __global__ void mul(int *vect , int (*mat)[SIZE][SIZE] , int *res){
 int main(){
     int vect[SIZE];
     int mat[SIZE][SIZE];
-    int result[SIZE];
+    long result[SIZE];
     cudaEvent_t gpu_start,gpu_stop;
     float gpu_elapsed_time;
 
     // pointer to gpu location
     int *d_in_vector,(*d_in_mat)[SIZE][SIZE];
-    int *d_out_result;
+    long *d_out_result;
 
     // intialize
     for(int i = 0 ; i < SIZE ; i++){
@@ -32,7 +32,7 @@ int main(){
     // Allocate memory to gpu
     cudaMalloc((void**)&d_in_vector,SIZE*sizeof(int));
     cudaMalloc((void**)&d_in_mat,SIZE*SIZE*sizeof(int));
-    cudaMalloc((void**)&d_out_result,SIZE*sizeof(int));
+    cudaMalloc((void**)&d_out_result,SIZE*sizeof(long));
 
     // Copy the contents to gpu
     cudaMemcpy(d_in_vector,vect,SIZE*sizeof(int),cudaMemcpyHostToDevice);
@@ -44,9 +44,10 @@ int main(){
     cudaEventRecord(gpu_start,0);
 
     // invoke the kernel function
-    mul<<<1,SIZE>>>(d_in_vector,d_in_mat,d_out_result);
+    int blk = SIZE/1024;
+    mul<<<blk+1,1024>>>(d_in_vector,d_in_mat,d_out_result);
     // cpy the result back 
-    cudaMemcpy(result,d_out_result,SIZE*sizeof(int),cudaMemcpyDeviceToHost);
+    cudaMemcpy(result,d_out_result,SIZE*sizeof(long),cudaMemcpyDeviceToHost);
     
     cudaEventRecord(gpu_stop,0);
     cudaEventSynchronize(gpu_stop);
@@ -65,7 +66,7 @@ int main(){
             result[i] += vect[j]*mat[j][i];  
     }
     clock_t endTime = clock();
-    printf("\n\nTime for sequential: %.4f",(float)(endTime-startTime)/CLOCKS_PER_SEC);
+    printf("\n\nTime for sequential: %.3f",(float)(endTime-startTime)/CLOCKS_PER_SEC);
     // print result
     // for(int i = 0 ; i < SIZE ; i++){
     //     cout<<result[i]<<" ";
